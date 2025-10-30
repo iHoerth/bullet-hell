4d6c4cd3571ea449960459ed4c2b1de66c41add0
Ignacio Hoerth <hoerth.ignacio@gmail.com>
2025-10-30 02:55:03 -0300

    Enemy Attack Animation!! (a little bugged XD) but works. Needs a time window to be fair, otherwise its insta damage

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,12 +5,17 @@ public class Enemy : MonoBehaviour
     public GameObject player;
     private PlayerController playerController;
     private Rigidbody rb;
-    
+    private Animator anim;
+
+
     public float speed = 5f;
     public int damage;
+    private bool isAttacking = false;
     public int health;
     public float attackRange = 3f;
 
+    float distanceToPlayer;
+    // Called once to initialize some variables
     void Awake()
     {
         health = 20;
@@ -18,29 +23,43 @@ public class Enemy : MonoBehaviour
     }
 
     void Start()
-    {   
+    {       
+
         // Since the enemy is a prefab, the player has to be searched in runtime as follows
         player = GameObject.FindWithTag("Player");
         playerController = player.GetComponent<PlayerController>();
         rb = GetComponent<Rigidbody>();
+
+        anim = GetComponent<Animator>();
     }
     // Update is called once per frame
     void FixedUpdate()
     {   
+        Move();
+
+
+        // Calculate distanceToPlayer to player
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if(distanceToPlayer <= attackRange)
+        {   
+            Attack();
+        }
+    }
+
+    // This is called every frame in FixedUpdate
+    void Move()
+    {
         if (player == null) return;
+
+        speed = isAttacking? 1f : 5f;
+
+        // Get Player position. This logic could be in a helper GetTargetPosition() but not sure
         Vector3 target = new Vector3(player.transform.position.x, transform.position.y , player.transform.position.z);
         Vector3 dir = (target - transform.position).normalized;
 
         transform.LookAt(target);
         transform.position += dir * speed * Time.deltaTime;
-
-        // Attack
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-
-        if(distance <= attackRange)
-        {
-            playerController.TakeDamage(damage);
-        }
     }
 
     public void TakeDamage(int damage)
@@ -56,15 +75,32 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    // This gets called by FixedUpdate, which cheks every frame for the distance to the player. If less than attackRadius, Attack is called.
     public void Attack()
     {   
-        // Vector3.distance returns a float with the distance between two R3 points (pythagoras)
+        anim.SetBool("isAttacking", true);
+        isAttacking = true;
+        // activar animación
+        // detener movimiento
+        // esperar duración * 0.33
+
+        if(distanceToPlayer <= attackRange)
+        {
+            playerController.TakeDamage(damage);
+        }
+    }
+
+    // This gets called by the Animator Event "EndAttack"
+    public void EndAttack()
+    {
+        anim.SetBool("isAttacking", false);
+        isAttacking = false;
     }
 
     void Die()
     {
-        // Animacion de muerte
-        // Sonido de muerte
+        // Animacion de muerte ???
+        // Sonido de muerte todavia no tengo
         Destroy(gameObject);
     }
 }
